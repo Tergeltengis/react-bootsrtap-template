c5002eef929e8740f6b69629823e746babed2c36
tergeltengis <tergeltengis77@gmail.com>
2022-05-09 10:53:54 +0800

    hahaha navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -34,7 +34,7 @@ export function NavBar() {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Grocery Store Tergeltengis
+              HAHAHAHAHAHA
             </Typography>
 
             <MaterialUISwitch
